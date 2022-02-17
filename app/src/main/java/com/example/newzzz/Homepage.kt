ebe3a29a4ba47bc4c85400436cc571dc50a37601
Vivek Devre <vivekdevre45@gmail.com>
2022-02-17 10:56:00 +0530

    app,icon changes

--- app/src/main/java/com/example/newzzz/Homepage.kt
+++ app/src/main/java/com/example/newzzz/Homepage.kt
@@ -3,16 +3,68 @@ package com.example.newzzz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_homepage.*
 
 
 class Homepage : AppCompatActivity() {
+    private var tempMenuItemClicked: Int = 0
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.india -> {
+                tempMenuItemClicked = 1
+                true
+            }
+            R.id.usa -> {
+                tempMenuItemClicked = 2
+                true
+            }
+            R.id.australia->{
+                tempMenuItemClicked = 3
+                true
+            }
+            R.id.russia->{
+                tempMenuItemClicked = 4
+                true
+            }
+            R.id.france->{
+                tempMenuItemClicked = 5
+                true
+            }
+            R.id.uk->{
+                tempMenuItemClicked = 6
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
         General.setOnClickListener {
-            val value = 1
+            var value = 10
+            when(tempMenuItemClicked){
+                1->value=11
+                2->value=12
+                3->value=13
+                4->value=14
+                5->value=15
+                6->value=16
+
+            }
+
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -21,7 +73,15 @@ class Homepage : AppCompatActivity() {
         }
 
         Business.setOnClickListener {
-            val value = 2
+            var value = 20
+            when(tempMenuItemClicked){
+                1->value=21
+                2->value=22
+                3->value=23
+                4->value=24
+                5->value=25
+                6->value=26
+            }
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -30,7 +90,15 @@ class Homepage : AppCompatActivity() {
         }
 
         Entertainment.setOnClickListener {
-            val value = 3
+            var value = 30
+            when(tempMenuItemClicked){
+                1->value=31
+                2->value=32
+                3->value=33
+                4->value=34
+                5->value=35
+                6->value=36
+            }
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -39,7 +107,15 @@ class Homepage : AppCompatActivity() {
         }
 
         Health.setOnClickListener {
-            val value = 4
+            var value = 40
+            when(tempMenuItemClicked){
+                1->value=41
+                2->value=42
+                3->value=43
+                4->value=44
+                5->value=45
+                6->value=46
+            }
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -48,7 +124,15 @@ class Homepage : AppCompatActivity() {
         }
 
         Science.setOnClickListener {
-            val value = 5
+            var value = 50
+            when(tempMenuItemClicked){
+                1->value=51
+                2->value=52
+                3->value=53
+                4->value=54
+                5->value=55
+                6->value=56
+            }
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -57,7 +141,15 @@ class Homepage : AppCompatActivity() {
         }
 
         Sports.setOnClickListener {
-            val value = 6
+            var value = 60
+            when(tempMenuItemClicked){
+                1->value=61
+                2->value=62
+                3->value=63
+                4->value=64
+                5->value=65
+                6->value=66
+            }
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -66,7 +158,15 @@ class Homepage : AppCompatActivity() {
         }
 
         Technology.setOnClickListener {
-            val value = 7
+            var value = 70
+            when(tempMenuItemClicked){
+                1->value=71
+                2->value=72
+                3->value=73
+                4->value=74
+                5->value=75
+                6->value=76
+            }
             val intent = Intent(this,MainActivity::class.java)
 
             intent.putExtra("key1",value)
@@ -74,4 +174,5 @@ class Homepage : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
--- app/src/main/java/com/example/newzzz/MainActivity.kt
+++ app/src/main/java/com/example/newzzz/MainActivity.kt
@@ -4,6 +4,9 @@ package com.example.newzzz
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
@@ -13,7 +16,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), NewsItemClicked {
     private lateinit var mAdapter: NewsListAdapter
     private var tempUrl:Int = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,35 +29,166 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
         tempUrl=intent.getIntExtra("key1",0)
     }
 
-
     private fun giveUrl():String{
         tempUrl=intent.getIntExtra("key1",0)
         var ans =""
 
         when (tempUrl){
-            1 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/general/in.json"
-                    title = "General News"
+            10 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/in.json"
+                title = "General News-India"
             }
-            2 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/in.json"
-                    title = "Business News"
+            11 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/in.json"
+                title = "General News-India"
             }
-            3 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/in.json"
-                    title = "Entertainment News"
+            12 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/us.json"
+                title = "General News-USA"
             }
-            4 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
-                    title = "Health News"
+            13 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/au.json"
+                title = "General News-Australia"
             }
-            5 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/in.json"
-                    title = "Science News"
+            14 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/ru.json"
+                title = "General News-Russia"
             }
-            6 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json"
-                    title = "Sports News"
+            15 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/fr.json"
+                title = "General News-France"
             }
-            7 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
-                    title = "Tech News"
+            16 ->
+            { ans="https://saurav.tech/NewsAPI/top-headlines/category/general/gb.json"
+                title = "General News-UnitedKingdom"
+            }
+            20 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/in.json"
+                    title = "Business News-India"
+            }
+            21 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/in.json"
+                    title = "Business News-India"
+            }
+            22 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+                    title = "Business News-USA"
+            }
+            23 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/au.json"
+                    title = "Business News-Australia"
+            }
+            24 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/ru.json"
+                    title = "Business News-Russia"
+            }
+            25 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/fr.json"
+                    title = "Business News-France"
+            }
+            26 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/business/gb.json"
+                    title = "Business News-UnitedKingdom"
+            }
+            30 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/in.json"
+                    title = "Entertainment News-India"
+            }
+            31 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/in.json"
+                    title = "Entertainment News-India"
+            }
+            32 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/us.json"
+                    title = "Entertainment News-USA"
+            }
+            33 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/au.json"
+                    title = "Entertainment News-Australia"
+            }
+            34 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/ru.json"
+                    title = "Entertainment News-Russia"
+            }
+            35 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/fr.json"
+                    title = "Entertainment News-France"
+            }
+            36 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/gb.json"
+                    title = "Entertainment News-UnitedKingdom"
+            }
+            40 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+                    title = "Health News-India"
+            }
+            41 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+                    title = "Health News-India"
+            }
+            42 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/us.json"
+                    title = "Health News-USA"
+            }
+            43 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/au.json"
+                    title = "Health News-Australia"
+            }
+            44 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/ru.json"
+                    title = "Health News-Russia"
+            }
+            45 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/fr.json"
+                    title = "Health News-France"
+            }
+            46 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/health/gb.json"
+                    title = "Health News-UnitedKingdom"
+            }
+            50 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/in.json"
+                    title = "Science News-India"
+            }
+            51 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/in.json"
+                    title = "Science News-India"
+            }
+            52 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/us.json"
+                    title = "Science News-USA"
+            }
+            53 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/au.json"
+                    title = "Science News-Australia"
+            }
+            54 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/ru.json"
+                    title = "Science News-Russia"
+            }
+            55 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/fr.json"
+                    title = "Science News-France"
+            }
+            56 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/science/gb.json"
+                    title = "Science News-UnitedKingdom"
+            }
+            60 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json"
+                    title = "Sports News-India"
+            }
+            61 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json"
+                    title = "Sports News-India"
+            }
+            62 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/us.json"
+                    title = "Sports News-USA"
+            }
+            63 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/au.json"
+                    title = "Sports News-Australia"
+            }
+            64 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/ru.json"
+                    title = "Sports News-Russia"
+            }
+            65 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/fr.json"
+                    title = "Sports News-France"
+            }
+            66 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/sports/gb.json"
+                    title = "Sports News-UnitedKingdom"
+            }
+            70 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+                    title = "Tech News-India"
+            }
+            71 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+                    title = "Tech News-India"
+            }
+            72 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json"
+                    title = "Tech News-USA"
+            }
+            73 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/au.json"
+                    title = "Tech News-Australia"
+            }
+            74 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/ru.json"
+                    title = "Tech News-Russia"
+            }
+            75 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/fr.json"
+                    title = "Tech News-France"
+            }
+            76 -> {ans="https://saurav.tech/NewsAPI/top-headlines/category/technology/gb.json"
+                    title = "Tech News-UnitedKingdom"
             }
         }
-
         return ans
     }
 
@@ -94,4 +227,5 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
         val customTabsIntent = builder.build()
         customTabsIntent.launchUrl(this, Uri.parse(item.url))
     }
+
 }
